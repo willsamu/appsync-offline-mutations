f2afba987d6ff11fbf25c8b1d5e6a1bda79460e0
willsamu <bok5@gmx.net>
2020-05-16 22:25:31 +0200

    Chore: Lint autofix

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify from "aws-amplify";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
 
-import config from "./aws-exports";
+import config from './aws-exports';
 
 Amplify.configure(config);
 Amplify.Analytics.disable(); // In order to remove Amplify Analytics No Credentials warning
@@ -11,7 +11,7 @@ Amplify.Analytics.disable(); // In order to remove Amplify Analytics No Credenti
 const App = () => {
   return (
     <View>
-      <Text>Let's get started!</Text>
+      <Text>Let&apos;s get started!</Text>
     </View>
   );
 };
