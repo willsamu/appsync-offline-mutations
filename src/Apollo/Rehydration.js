cdf1516d5d9deb045382e26ee6c50345cb29a996
willsamu <bok5@gmx.net>
2020-05-17 05:54:20 +0200

    Refactor: Rehydration Best Practices

--- src/Apollo/Rehydration.js
+++ src/Apollo/Rehydration.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useApolloClient } from 'react-apollo';
+import { ApolloClientOffline } from '@wora/apollo-offline/lib/ApolloClientOffline';
 
 const Rehydration = ({ children }) => {
   const client = useApolloClient();
   const [rehydrated, setRehydrated] = useState(false);
 
   useEffect(() => {
-    client.hydrate().then(() => setRehydrated(true));
-  }, []);
+    // eslint-disable-next-line no-unused-expressions
+    client instanceof ApolloClientOffline && client.hydrate().then(() => setRehydrated(true));
+  }, [client]);
   return rehydrated ? <>{children}</> : null;
 };
 
