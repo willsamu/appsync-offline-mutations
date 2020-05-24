211ef740a4c142de739b3d51f1d1df0fa1be1ea6
Samuel <bok5@gmx.net>
2020-05-24 17:37:48 +0200

    Chore: update onMutationCompleteHandler comments

--- src/Apollo/onMutationCompleteHandler.js
+++ src/Apollo/onMutationCompleteHandler.js
@@ -3,18 +3,15 @@ import { handleUpdateTodo } from '../TestComponent';
 const onMutationCompleteHandler = (client, response) => {
   if (response && response.errors) {
     console.log(`ERROR FOR MUTATION: \n${response.errors}`);
-    /**
+     /**
      * ERROR HANDLING
-     * Procceed depending on error-type, for ConditinalCheckFailedExceptions
-     * implement something like:
-     *
-     * updateCacheEntry(response.errors[0]);
-     *
+     * could implement update / remove cache entry with
+     * 'errorHandler(client, response.errors);'
      *  */
   } else if (response && response.data) {
     switch (Object.keys(response.data).pop()) {
       case 'createTodo': {
-        handleUpdateTodo(client, response);
+        handleUpdateTodo(client, response); // make sure to not add entry twice in handleUpdateTodo
         break;
       }
       default:
